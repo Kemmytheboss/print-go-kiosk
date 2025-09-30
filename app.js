9162a3ce8746a9903c60d64f4e07d474bdc40d40
Verah Mokaya <verahmokaya475@gmail.com>
2025-09-30 21:26:39 +0300

    created fecth request to enableclients to uplad files and cfreated event listener

---
+++ app.js
@@ -0,0 +1,31 @@
+const fileInput = document.querySelector("#fileInput");
+const Preview = document.querySelector("#preview")
+const priceTag = document.querySelector("#price");
+const copiesInput = document.querySelector("#copies");
+const uploads = document.querySelector("#uploadForm");
+
+uploads.addEventListener('submit', 
+    async (e) =>{
+    e.preventDefault();
+    
+    const formData = new formData();
+    formData.append('file', fileInput.files[0]);
+
+    let res = await fetch('/upload', 
+        {
+            method: "POST", 
+            body: formData
+        }
+    )
+    let data = await res.json();
+    Preview.innerHTML = `<p>Uploaded: ${data.filename}</p>`
+
+    copiesInput.addEventListener('input', () =>{
+        let copies = copiesInput.value;
+        priceTag.textContent = 'KES' + (copies * 5);
+    });
+
+    function pay (method) {
+        document.querySelector(#paymentStatus)
+    }
+})
