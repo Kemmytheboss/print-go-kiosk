531b3c5567a39717b882a0ff84bde640e2f5215c
Verah Mokaya <verahmokaya475@gmail.com>
2025-09-30 22:29:58 +0300

    added post method to store cleints data to dbjson

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const copiesInput = document.querySelector("#copies");
 const uploads = document.querySelector("#uploadForm");
 const payment = document.querySelector("paymentStatus");
 
+
 uploads.addEventListener('submit', 
     async (e) =>{
     e.preventDefault();
@@ -27,6 +28,27 @@ uploads.addEventListener('submit',
     });
 
     function pay (method) {
-        payment.innerText = `Processing ${method} payment...(simulation)`;
-    }
+        const file = fileInput.files[0];
+        const copies = document.getElementById('copies').value;
+        const color = document.getElementById('color').value;
+        const price = copies * 5;
+
+        // save transaction to db.json
+        let transaction = {
+            filename: file ? filename : 'No File',
+            copies : copies,
+            color : color,
+            price : price,
+            paymentMethod : method,
+            timeStamp : new Date().toISOString()
+        };
+
+        await fecth ('http://localhost:5000/transactions', {
+            method: 'POST',
+            Headers : {'content-type' : 'application/json'},
+            body : JSON.stringfy(transaction)
+        });
+        document.getElementById('paymentStatus')innerText = 
+        `payment via ${method} successful. Transaction saved.`
+        }
 })
