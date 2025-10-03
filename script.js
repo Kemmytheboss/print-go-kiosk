914964052f0b9130b0d443c71393b4e01d6b6660
Verah Mokaya <verahmokaya475@gmail.com>
2025-10-03 21:29:23 +0300

    created fecth function to get catalog from dbjson

--- script.js
+++ script.js
@@ -1,54 +1,21 @@
-const fileInput = document.querySelector("#fileInput");
-const Preview = document.querySelector("#preview")
-const priceTag = document.querySelector("#price");
-const copiesInput = document.querySelector("#copies");
-const uploads = document.querySelector("#uploadForm");
-const payment = document.querySelector("paymentStatus");
+// fetching catalog from db.json
+async function loadCatalog () {
+    const res = await fetch("db.json");
+    const data = await res.json();
+    const catalogDiv = document.getElementById("catalog");
 
+    if (catalogDiv) {
+        catalogDiv.innerHTML = data.services.map(
+            service => `
+                <div class="card"
+                    <img src="${service.image}" alt="${service.name} width="100%">
+                    <h3>${service.name}</h3>
+                    <p>${service.description}></p>
+                    <p><strong>${service.price}</strong></p>
+                    <a href="product.html ? id=${service.id}" class="btn">View</a>
+                </div>
+                `
+             ).join("");
+    }
 
-uploads.addEventListener('submit', 
-    async (e) =>{
-    e.preventDefault();
-    
-    const formData = new formData();
-    formData.append('file', fileInput.files[0]);
-
-    let res = await fetch('/upload', 
-        {
-            method: "POST", 
-            body: formData
-        }
-    )
-    let data = await res.json();
-    Preview.innerHTML = `<p>Uploaded: ${data.filename}</p>`
-
-    copiesInput.addEventListener('input', () =>{
-        let copies = copiesInput.value;
-        priceTag.textContent = 'KES' + (copies * 5);
-    });
-
-    async function pay(method) {
-        const file = fileInput.files[0];
-        const copies = document.getElementById('copies').value;
-        const color = document.getElementById('color').value;
-        const price = copies * 5;
-
-        // save transaction to db.json
-        let transaction = {
-            filename: file ? filename : 'No File',
-            copies : copies,
-            color : color,
-            price : price,
-            paymentMethod : method,
-            timeStamp : new Date().toISOString()
-        };
-
-        await fecth ('http://localhost:5000/transactions', {
-            method: 'POST',
-            Headers : {'content-type' : 'application/json'},
-            body : JSON.stringfy(transaction)
-        });
-        document.getElementById('paymentStatus').innerText = 
-        `payment via ${method} successful. Transaction saved.`
-        }
-})
+}
